9f7f58fdcdc91e43606410f7c5b49f5d095af4ee
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-12-22 13:10:33 -0800

    :memo:

--- lib/staff_notes/markdown.ex
+++ lib/staff_notes/markdown.ex
@@ -6,7 +6,39 @@ defmodule StaffNotes.Markdown do
 
   [beyond-functions]: https://blog.usejournal.com/beyond-functions-in-elixir-refactoring-for-maintainability-5c73daba77f3
   """
+  @type t :: %__MODULE__{text: String.t, html: nil | String.t}
   defstruct text: "", html: nil
+
+  @type markdown :: %__MODULE__{} | String.t
+
+  @doc """
+  Renders the supplied Markdown as HTML.
+
+  ## Examples
+
+  Render Markdown from a string:
+
+  ```
+  iex> StaffNotes.Markdown.to_html("# Foo")
+  "<h1>Foo</h1>\n"
+  ```
+
+  Render Markdown from a `StaffNotes.Markdown` struct:
+
+  ```
+  iex> StaffNotes.Markdown.to_html(%StaffNotes.Markdown{text: "# Foo"})
+  "<h1>Foo</h1>\n"
+  ```
+
+  Passes HTML through unchanged:
+
+  ```
+  iex> StaffNotes.Markdown.to_html(%StaffNotes.Markdown{html: "<p>foo</p>"})
+  "<p>foo</p>"
+  ```
+  """
+  @spec to_html(markdown) :: String.t
+  def to_html(markdown)
 
   def to_html(%__MODULE__{html: html}) when is_binary(html), do: html
   def to_html(%__MODULE__{text: text}), do: to_html(text)
