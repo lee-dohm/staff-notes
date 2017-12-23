0e3f142fdd560ab8f457f8f246bf704be4a86d18
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-12-22 16:38:34 -0800

    Flesh out the ErrorView specs

--- spec/staff_notes_web/views/error_view_spec.exs
+++ spec/staff_notes_web/views/error_view_spec.exs
@@ -9,26 +9,20 @@ defmodule StaffNotesWeb.ErrorViewSpec do
 
     it do: expect(content()).to have("Page not found")
   end
-end
 
-# defmodule StaffNotesWeb.ErrorViewTest do
-#   use StaffNotesWeb.ConnCase, async: true
-#
-#   # Bring render/3 and render_to_string/3 for testing custom views
-#   import Phoenix.View
-#
-#   test "renders 404.html" do
-#     assert render_to_string(StaffNotesWeb.ErrorView, "404.html", []) ==
-#            "Page not found"
-#   end
-#
-#   test "render 500.html" do
-#     assert render_to_string(StaffNotesWeb.ErrorView, "500.html", []) ==
-#            "Internal server error"
-#   end
-#
-#   test "render any other" do
-#     assert render_to_string(StaffNotesWeb.ErrorView, "505.html", []) ==
-#            "Internal server error"
-#   end
-# end
+  describe "500.html" do
+    let :content do
+      render_to_string(StaffNotesWeb.ErrorView, "500.html", conn: build_conn())
+    end
+
+    it do: expect(content()).to have("Internal server error")
+  end
+
+  describe "any other" do
+    let :content do
+      render_to_string(StaffNotesWeb.ErrorView, "505.html", conn: build_conn())
+    end
+
+    it do: expect(content()).to have("Internal server error")
+  end
+end
