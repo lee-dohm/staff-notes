dfbdde2f32cd63ac808b5acf4887f83eb471db5e
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-12-24 09:27:22 -0800

    :memo:

--- lib/iex_helpers.ex
+++ lib/iex_helpers.ex
@@ -13,13 +13,29 @@ defmodule IExHelpers do
 
   @doc """
   Demotes the named user to a normal non-admin account.
+
+  Raises an `Ecto.NoResultsError` if the named user cannot be found.
   """
-  def demote_user(name) when is_binary(name), do: name |> Accounts.get_user! |> demote_user
+  @spec demote_user!(String.t) :: User.t | no_return
+  def demote_user!(name) when is_binary(name), do: name |> Accounts.get_user! |> demote_user
+
+  @doc """
+  Demotes the given user to a normal non-admin account.
+  """
+  @spec demote_user(User.t) :: User.t
   def demote_user(%User{} = user), do: Accounts.update_user(user, %{site_admin: false})
 
   @doc """
   Promotes the named user to a site admin.
+
+  Raises an `Ecto.NoResultsError` if the named user cannot be found.
   """
-  def promote_user(name) when is_binary(name), do: name |> Accounts.get_user! |> promote_user
+  @spec promote_user!(String.t | User.t) :: User.t | no_return
+  def promote_user!(name) when is_binary(name), do: name |> Accounts.get_user! |> promote_user
+
+  @doc """
+  Promotes the given user to a site admin.
+  """
+  @spec promote_user(User.t) :: User.t
   def promote_user(%User{} = user), do: Accounts.update_user(user, %{site_admin: true})
 end
