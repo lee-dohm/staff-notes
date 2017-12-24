34c76704b766da51a7e07c840dd29d104d4b06de
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-12-24 09:04:23 -0800

    Add IEx helpers to promote and demote a user

--- lib/iex_helpers.ex
+++ lib/iex_helpers.ex
@@ -3,8 +3,23 @@ defmodule IExHelpers do
   Helper functions for use in `IEx`.
   """
 
+  alias StaffNotes.Accounts
+  alias StaffNotes.Accounts.User
+
   @doc """
   Cleanly shuts down the local node and exits the shell.
   """
   def exit, do: System.stop()
+
+  @doc """
+  Demotes the named user to a normal non-admin account.
+  """
+  def demote_user(name) when is_binary(name), do: name |> Accounts.get_user! |> demote_user
+  def demote_user(%User{} = user), do: Accounts.update_user(user, %{site_admin: false})
+
+  @doc """
+  Promotes the named user to a site admin.
+  """
+  def promote_user(name) when is_binary(name), do: name |> Accounts.get_user! |> promote_user
+  def promote_user(%User{} = user), do: Accounts.update_user(user, %{site_admin: true})
 end
