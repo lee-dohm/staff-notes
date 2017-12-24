b0402ad4f7f346002e0a3c10ec25519cf457c0ec
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-12-23 18:59:42 -0800

    Add common aliases to .iex.exs

---
+++ .iex.exs
@@ -0,0 +1,12 @@
+alias StaffNotes.Accounts
+alias StaffNotes.Accounts.User
+alias StaffNotes.Markdown
+alias StaffNotes.Repo
+alias StaffNotesWeb.AuthController
+alias StaffNotesWeb.PageController
+alias StaffNotesWeb.UserController
+alias StaffNotesWeb.AvatarHelpers
+alias StaffNotesWeb.ErrorView
+alias StaffNotesWeb.LayoutView
+alias StaffNotesWeb.PageView
+alias StaffNotesWeb.UserView
