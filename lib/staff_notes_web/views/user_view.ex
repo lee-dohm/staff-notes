9a296945d05d4d047c1d9d3e3a0ecedcc0828d24
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-12-23 20:59:30 -0800

    :memo:

--- lib/staff_notes_web/views/user_view.ex
+++ lib/staff_notes_web/views/user_view.ex
@@ -4,9 +4,13 @@ defmodule StaffNotesWeb.UserView do
   """
   use StaffNotesWeb, :view
 
+  alias StaffNotes.Accounts.User
+
   @doc """
   Displays a staff badge if the given user is a site admin.
   """
+  @spec staff_badge(User.t, Keyword.t) :: Phoenix.HTML.safe
+  def staff_badge(user, options)
   def staff_badge(%{site_admin: true}, options), do: content_tag(:span, gettext("Staff"), options)
   def staff_badge(_, _), do: ""
 end
