568a9b69670ba66da2f85f6d05cdff765153a2ab
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-12-23 20:55:23 -0800

    Refactor the user view

--- lib/staff_notes_web/views/user_view.ex
+++ lib/staff_notes_web/views/user_view.ex
@@ -1,3 +1,12 @@
 defmodule StaffNotesWeb.UserView do
+  @moduledoc """
+  View functions for the user pages.
+  """
   use StaffNotesWeb, :view
+
+  @doc """
+  Displays a staff badge if the given user is a site admin.
+  """
+  def staff_badge(%{site_admin: true}, options), do: content_tag(:span, gettext("Staff"), options)
+  def staff_badge(_, _), do: ""
 end
