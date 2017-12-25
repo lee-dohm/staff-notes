2cf99f7b4cff0c7f953ed5fe9d79ea4856dee864
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-12-25 12:43:22 -0800

    Separate the schema types into contexts

--- lib/staff_notes.ex
+++ lib/staff_notes.ex
@@ -2,22 +2,26 @@ defmodule StaffNotes do
   @moduledoc """
   Staff Notes is an application that keeps track of these things:
 
-  1. Organizations
-      * An organization has many users
-      * An organization has many members
-  1. Users &mdash; the staff of an organization
-      * A user belongs to an organization
-      * A user authors many notes
-  1. Members &mdash; the members of the organization the staff notes are about
-      * Members belong to an organization
-      * A member has many notes
-      * A member has many identities
-  1. Notes &mdash; the notes about the members
-      * A note belongs to a member
-      * A note was authored by a user
-      * Notes may include text, images or other files
-  1. Identities &mdash; information identifying a member on a service
-      * An identity belongs to a member
+  * Accounts
+      * Organizations
+          * An organization has many users
+      * Teams
+          * A team belongs to an organization
+          * A team can have many users
+      * Users &mdash; the staff of an organization
+          * A user can be on many teams
+          * A user authors many notes
+  * Records
+      * Members &mdash; the members of the organization the staff notes are about
+          * Members belong to an organization
+          * A member has many notes
+          * A member has many identities
+      * Notes &mdash; the notes about the members
+          * A note belongs to a member
+          * A note was authored by a user
+          * Notes may include text, images or other files
+      * Identities &mdash; information identifying a member on a service
+          * An identity belongs to a member
 
   Because the Internet is a strange and fluid place, members may have different names on different
   platforms. For example, a member may be named "foo" on GitHub but named "bar" on the message
