f83707c4883e74c56a7ebcc45600b003997f0740
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-12-27 12:07:20 -0800

    Split out Organization specs into their own module

--- spec/staff_notes/accounts/accounts_spec.exs
+++ spec/staff_notes/accounts/accounts_spec.exs
@@ -2,13 +2,13 @@ defmodule StaffNotes.AccountsSpec do
   use ESpec
 
   alias StaffNotes.Accounts
-  alias StaffNotes.Accounts.Organization
+  alias StaffNotes.Accounts.Team
   alias StaffNotes.Accounts.User
 
   def org_fixture(attrs \\ %{}) do
     {:ok, org} =
       attrs
-      |> Enum.into(valid_attrs())
+      |> Enum.into(valid_org_attrs())
       |> Accounts.create_org()
 
     org
@@ -19,7 +19,7 @@ defmodule StaffNotes.AccountsSpec do
 
     {:ok, team} =
       attrs
-      |> Enum.into(valid_attrs())
+      |> Enum.into(valid_team_attrs())
       |> Accounts.create_team(org)
 
     team
@@ -34,93 +34,22 @@ defmodule StaffNotes.AccountsSpec do
     user
   end
 
-  describe "organizations" do
-    let :valid_attrs, do: %{name: "some name"}
-    let :update_attrs, do: %{name: "some updated name"}
-    let :invalid_attrs, do: %{name: nil}
+  describe "original_team/1" do
+    let :original_team_attrs, do: Team.original_team_attrs()
+    let :original_team, do: team_fixture(original_team_attrs())
+    let :valid_team_attrs, do: %{name: "team name", permission: :write, original: false}
+    let :valid_org_attrs, do: %{name: "org name"}
 
-    describe "list_orgs/0" do
-      it "returns all orgs" do
-        org = org_fixture()
+    it "returns the original team when one exists" do
+      team = original_team()
 
-        expect(Accounts.list_orgs()).to eq([org])
-      end
+      expect(Accounts.original_team(team.organization_id)).to eq(team)
     end
 
-    describe "get_org!/1" do
-      it "returns the org with the given id" do
-        org = org_fixture()
+    it "returns nil when an original team does not exist" do
+      team = team_fixture(%{name: "some name", permission: :owner, original: false})
 
-        expect(Accounts.get_org!(org.id)).to eq(org)
-      end
-
-      it "raises an exception when given an invalid id" do
-        expect(fn ->
-          Accounts.get_org!(Ecto.UUID.generate())
-        end).to raise_exception(Ecto.NoResultsError)
-      end
-    end
-
-    describe "create_org/1" do
-      it "creates an org when given valid information" do
-        {:ok, %Organization{} = org} = Accounts.create_org(valid_attrs())
-
-        expect(org.name).to eq("some name")
-      end
-
-      it "returns an error changeset when given invalid data" do
-        {:error, changeset} = Accounts.create_org(invalid_attrs())
-
-        expect(changeset).to be_struct(Ecto.Changeset)
-      end
-    end
-
-    describe "update_org/2" do
-      it "updates the org when given valid data" do
-        org = org_fixture()
-        {:ok, org} = Accounts.update_org(org, update_attrs())
-
-        expect(org.name).to eq("some updated name")
-      end
-
-      it "returns an error changeset when given invalid data" do
-        org = org_fixture()
-        {:error, changeset} = Accounts.update_org(org, invalid_attrs())
-
-        expect(changeset).to be_struct(Ecto.Changeset)
-        expect(Accounts.get_org!(org.id)).to eq(org)
-      end
-    end
-
-    describe "delete_org/1" do
-      it "deletes the given org" do
-        org = org_fixture()
-        {:ok, %Organization{}} = Accounts.delete_org(org)
-
-        expect(fn -> Accounts.get_org!(org.id) end).to raise_exception(Ecto.NoResultsError)
-      end
-    end
-
-    describe "change_org/1" do
-      it "returns a org changeset" do
-        org = org_fixture()
-
-        expect(Accounts.change_org(org)).to be_struct(Ecto.Changeset)
-      end
-    end
-
-    describe "original_team/1" do
-      it "returns the original team when one exists" do
-        team = team_fixture(%{name: "some name", permission: :owner, original: true})
-
-        expect(Accounts.original_team(team.organization_id)).to eq(team)
-      end
-
-      it "returns nil when an original team does not exist" do
-        team = team_fixture(%{name: "some name", permission: :owner, original: false})
-
-        expect(Accounts.original_team(team.organization_id)).to be_nil()
-      end
+      expect(Accounts.original_team(team.organization_id)).to be_nil()
     end
   end
 
