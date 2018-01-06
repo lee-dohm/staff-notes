d947a00fe5051b37710515f1900249124ba4fa87
lee-dohm <1038121+lee-dohm@users.noreply.github.com>
2018-01-06 15:43:56 -0800

    Stub out all of the other resource actions

--- lib/staff_notes_web/controllers/organization_controller.ex
+++ lib/staff_notes_web/controllers/organization_controller.ex
@@ -8,7 +8,35 @@ defmodule StaffNotesWeb.OrganizationController do
   alias StaffNotes.Repo
 
   @doc """
-  Display the profile page for the organization.
+  Receives parameters for creating a new organization and saves it in the database.
+  """
+  def create(conn, _params) do
+    conn
+  end
+
+  @doc """
+  Receives an organization to be deleted by id and deletes it from the database.
+  """
+  def delete(conn, _params) do
+    conn
+  end
+
+  @doc """
+  Receives an organization by id and renders a form for editing it.
+  """
+  def edit(conn, _params) do
+    conn
+  end
+
+  @doc """
+  Renders a form for creating a new organization.
+  """
+  def new(conn, _params) do
+    conn
+  end
+
+  @doc """
+  Renders an organization by id.
   """
   @spec show(Plug.Conn.t, StaffNotesWeb.params) :: Plug.Conn.t
   def show(conn, params)
@@ -21,5 +49,12 @@ defmodule StaffNotesWeb.OrganizationController do
     conn
     |> assign(:org, org)
     |> render("show.html")
+  end
+
+  @doc """
+  Receives paramets for updating an organization and saves it to the database.
+  """
+  def update(conn, _params) do
+    conn
   end
 end
