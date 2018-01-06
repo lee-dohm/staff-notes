5299e5b925a199a4424d8ab708bdcd15f8797fa8
lee-dohm <1038121+lee-dohm@users.noreply.github.com>
2018-01-05 18:05:56 -0800

    Add tests for the user block on the UserView

--- test/staff_notes_web/views/user_view_test.exs
+++ test/staff_notes_web/views/user_view_test.exs
@@ -1,8 +1,11 @@
 defmodule StaffNotesWeb.UserViewTest do
-  use ExUnit.Case
+  use StaffNotesWeb.ConnCase
   use Phoenix.HTML
 
   alias StaffNotesWeb.UserView
+
+  import Phoenix.View
+  import StaffNotes.Support.Helpers
 
   def staff_badge(user, options \\ []) do
     user
@@ -21,6 +24,27 @@ defmodule StaffNotesWeb.UserViewTest do
 
     test "includes the options on the span tag" do
       assert staff_badge(%{site_admin: true}, foo: "bar") =~ ~s(<span foo="bar">)
+    end
+  end
+
+  describe "user block" do
+    setup do
+      {
+        :ok,
+        user: user_fixture()
+      }
+    end
+
+    test "displays the user's avatar", context do
+      content = render_to_string(UserView, "show.html", conn: context.conn, user: context.user)
+
+      assert content =~ ~s(<img class="avatar")
+    end
+
+    test "displays the user's name", context do
+      content = render_to_string(UserView, "show.html", conn: context.conn, user: context.user)
+
+      assert content =~ context.user.name
     end
   end
 end
