f7d4c805c362f9e6b07afa085acef30b62d81995
lee-dohm <1038121+lee-dohm@users.noreply.github.com>
2018-01-14 15:41:01 -0800

    Create and enforce name uniqueness constraint

---
+++ priv/repo/migrations/20180114233506_unique_names.exs
@@ -0,0 +1,8 @@
+defmodule StaffNotes.Repo.Migrations.UniqueNames do
+  use Ecto.Migration
+
+  def change do
+    # Ensures that each team name is unique within its organization
+    create unique_index(:teams, [:organization_id, :name])
+  end
+end
