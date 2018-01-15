df70bd3e7c0c4af72ddbe0d517620899ffe24ddb
lee-dohm <1038121+lee-dohm@users.noreply.github.com>
2018-01-14 20:27:51 -0800

    Fix up Staff index view

--- lib/staff_notes_web/views/staff_view.ex
+++ lib/staff_notes_web/views/staff_view.ex
@@ -3,4 +3,8 @@ defmodule StaffNotesWeb.StaffView do
   View functions for displaying `StaffNotes.Accounts.User` records as part of an organization.
   """
   use StaffNotesWeb, :view
+
+  def render_staff_info_list(conn, users) do
+    render_many(users, __MODULE__, "staff_info_item.html", conn: conn, as: :user)
+  end
 end
