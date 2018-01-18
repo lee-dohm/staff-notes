2899e8d87481f6f3d978a95cb5b08a716651fa12
lee-dohm <1038121+lee-dohm@users.noreply.github.com>
2018-01-17 20:23:42 -0800

    Add create staff note button

--- lib/staff_notes_web/views/organization_view.ex
+++ lib/staff_notes_web/views/organization_view.ex
@@ -3,4 +3,10 @@ defmodule StaffNotesWeb.OrganizationView do
   View functions for `StaffNotes.Accounts.Organization` templates.
   """
   use StaffNotesWeb, :view
+
+  def create_note_button(conn, org, options \\ []) do
+    link_options = Keyword.merge(options, to: organization_note_path(conn, :new, org), type: "button")
+
+    link(gettext("Create staff note"), link_options)
+  end
 end
