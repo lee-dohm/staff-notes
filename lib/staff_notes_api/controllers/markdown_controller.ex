27cec8f0015524f3ebde10c9c748592ea82b2226
lee-dohm <1038121+lee-dohm@users.noreply.github.com>
2018-01-20 17:13:15 -0800

    :memo:

--- lib/staff_notes_api/controllers/markdown_controller.ex
+++ lib/staff_notes_api/controllers/markdown_controller.ex
@@ -8,6 +8,22 @@ defmodule StaffNotesApi.MarkdownController do
 
   @doc """
   Renders the Markdown received and returns an HTML fragment.
+
+  ## Examples
+
+  Accepts a JSON body:
+
+  ```json
+  {
+    "markdown": "# Foo"
+  }
+  ```
+
+  Returns the equivalent HTML fragment:
+
+  ```html
+  <h1>Foo</h1>
+  ```
   """
   def render(conn, %{"markdown" => markdown}) do
     text(conn, Markdown.to_html(markdown))
