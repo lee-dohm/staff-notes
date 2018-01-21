b2988436edcc46500c35ff663f24e95718a6140a
lee-dohm <1038121+lee-dohm@users.noreply.github.com>
2018-01-20 22:27:45 -0800

    :shirt:

--- lib/staff_notes_web/helpers/link_helpers.ex
+++ lib/staff_notes_web/helpers/link_helpers.ex
@@ -1,4 +1,7 @@
 defmodule StaffNotesWeb.LinkHelpers do
+  @moduledoc """
+  View helper functions for rendering links of various kinds.
+  """
   use StaffNotesWeb, :helper
 
   import StaffNotesWeb.Router.Helpers
@@ -7,12 +10,18 @@ defmodule StaffNotesWeb.LinkHelpers do
   alias StaffNotes.Notes.Member
   alias StaffNotes.Repo
 
+  @doc """
+  Renders a link to a non-staff member's show page.
+  """
   def member_link(conn, %Member{} = member) do
     member = Repo.preload(member, [:organization])
 
     link(member.name, to: organization_member_path(conn, :show, member.organization, member), class: "member-link link-gray")
   end
 
+  @doc """
+  Renders a link to a staff member's show page.
+  """
   def staff_link(conn, %User{} = user) do
     link(user.name, to: user_path(conn, :show, user), class: "staff-link link-gray")
   end
