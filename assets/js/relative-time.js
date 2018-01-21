260bc642031aa2c13056442379f657686dc61401
lee-dohm <1038121+lee-dohm@users.noreply.github.com>
2018-01-20 21:27:20 -0800

    Fix up relative-time title attributes with locale-corrected text

--- assets/js/relative-time.js
+++ assets/js/relative-time.js
@@ -1,3 +1,16 @@
 const timeago = require('timeago.js')
 
-timeago().render(document.querySelectorAll('relative-time'))
+function timestampsToLocale(nodes) {
+  for (let node of nodes) {
+    let title = node.getAttribute('title')
+    if (title && title.length > 0) {
+      let date = new Date(title)
+
+      node.setAttribute('title', date.toLocaleString())
+    }
+  }
+}
+
+let nodes = document.querySelectorAll('relative-time')
+timeago().render(nodes)
+timestampsToLocale(nodes)
