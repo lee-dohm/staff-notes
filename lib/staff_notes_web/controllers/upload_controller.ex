2fc595e0c7731930e3573fe257eaff0c3b613908
lee-dohm <1038121+lee-dohm@users.noreply.github.com>
2018-01-22 09:22:45 -0800

    Add some logging

--- lib/staff_notes_web/controllers/upload_controller.ex
+++ lib/staff_notes_web/controllers/upload_controller.ex
@@ -1,6 +1,8 @@
 defmodule StaffNotesWeb.UploadController do
   use StaffNotesWeb, :controller
 
+  require Logger
+
   def upload_form(conn, _params) do
     render(conn, "upload.html")
   end
@@ -11,10 +13,13 @@ defmodule StaffNotesWeb.UploadController do
     filename = "#{uuid}.#{extension}"
     bucket = "staffnotes-io"
     {:ok, binary} = File.read(file.path)
-    {:ok, _} =
+
+    {:ok, _} = result =
       bucket
       |> ExAws.S3.put_object(filename, binary)
       |> ExAws.request()
+
+    Logger.debug(fn -> "File uploaded: #{inspect(result)}" end)
 
     conn
     |> put_flash(:info, "File uploaded successfully!")
