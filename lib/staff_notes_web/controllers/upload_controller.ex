76c276caae116ef592eadb3e290bbd25ef0e90e1
lee-dohm <1038121+lee-dohm@users.noreply.github.com>
2018-01-22 17:47:41 -0800

    Allow the upload form to upload more than one file

--- lib/staff_notes_web/controllers/upload_controller.ex
+++ lib/staff_notes_web/controllers/upload_controller.ex
@@ -7,22 +7,36 @@ defmodule StaffNotesWeb.UploadController do
     render(conn, "upload.html")
   end
 
-  def upload(conn, %{"upload" => %{"file" => file}}) do
+  def upload(conn, %{"upload" => %{"file" => files}}) when is_list(files) do
+    upload_files(files)
+
+    render(conn, "upload.html")
+  end
+
+  def upload(conn, %{"upload" => %{"file" => %Plug.Upload{} = file}}) do
+    upload_files([file])
+
+    render(conn, "upload.html")
+  end
+
+  defp upload_files(files) do
+    files
+    |> Enum.map(&(upload_file(&1)))
+    |> Enum.map(fn(url) -> Logger.debug(fn -> "File uploaded to #{url}" end) end)
+  end
+
+  defp upload_file(%Plug.Upload{} = file) do
     extension = Path.extname(file.filename)
     uuid = Ecto.UUID.generate()
     filename = "#{uuid}.#{extension}"
     bucket = "staffnotes-io"
     {:ok, binary} = File.read(file.path)
 
-    {:ok, _} = result =
+    {:ok, _} =
       bucket
       |> ExAws.S3.put_object(filename, binary)
       |> ExAws.request()
 
-    Logger.debug(fn -> "File uploaded: #{inspect(result)}" end)
-
-    conn
-    |> put_flash(:info, "File uploaded successfully!")
-    |> render("upload.html")
+    "https://#{bucket}.s3.amazonaws.com/#{filename}"
   end
 end
