15d993f2495cde74f850f83c382e8ac3f52799d6
lee-dohm <1038121+lee-dohm@users.noreply.github.com>
2018-01-24 20:01:17 -0800

    Fix all the stupid type checks

--- assets/js/drop-image.ts
+++ assets/js/drop-image.ts
@@ -1,17 +1,23 @@
-const imageDropElement = document.querySelector('.image-drop')
+type DataTransferEvent = CustomEvent & { dataTransfer?: DataTransfer }
+type FileReaderEvent = CustomEvent & { target: FileReader }
+type XMLHttpRequestEvent = CustomEvent & { target: XMLHttpRequest }
+
+const imageDropElement: HTMLTextAreaElement | null = document.querySelector('.image-drop')
 const submitButton = document.querySelector('.form-actions button[type="submit"]')
 
 /**
  * Callback to add styles indicating that the element can accept dropped data when hovering over it.
  */
-function addDragHover (event) {
-  imageDropElement.classList.add('is-drag-hover')
+function addDragHover(event: Event): void {
+  if (imageDropElement) {
+    imageDropElement.classList.add('is-drag-hover')
+  }
 }
 
 /**
  * Convert an `ArrayBuffer` to a base64-encoded `String`.
  */
-function arrayBufferToBase64 (buffer) {
+function arrayBufferToBase64(buffer: ArrayBuffer): string {
   let binary = ''
   let bytes = new Uint8Array(buffer)
   let len = bytes.byteLength
@@ -26,18 +32,24 @@ function arrayBufferToBase64 (buffer) {
 /**
  * Callback for when files are dropped in the target element.
  */
-async function drop (event) {
+async function drop(event: DataTransferEvent): Promise<void> {
   event.preventDefault()
 
-  submitButton.setAttribute('disabled', 'disabled')
-  await uploadFiles(event.dataTransfer.files)
-  submitButton.removeAttribute('disabled')
+  if (submitButton) {
+    submitButton.setAttribute('disabled', 'disabled')
+
+    if (event.dataTransfer) {
+      await uploadFiles(event.dataTransfer.files)
+    }
+
+    submitButton.removeAttribute('disabled')
+  }
 }
 
 /**
  * Inserts placeholder text into the `HTMLTextAreaElement` while the file is uploading.
  */
-function insertPlaceholder (el, filename) {
+function insertPlaceholder(el: HTMLTextAreaElement, filename: string): void {
   let pos = el.selectionStart > el.selectionEnd ? el.selectionStart : el.selectionEnd
   let oldText = el.value
   let newText = oldText.substring(0, pos) + `![Uploading ${filename}...]()` + oldText.substring(pos)
@@ -48,18 +60,20 @@ function insertPlaceholder (el, filename) {
 /**
  * Callback to remove drop styles from the target element.
  */
-function removeDragHover (event) {
-  imageDropElement.classList.remove('is-drag-hover')
+function removeDragHover(event: Event): void {
+  if (imageDropElement) {
+    imageDropElement.classList.remove('is-drag-hover')
+  }
 }
 
 /**
  * Reads a `File` and returns an `ArrayBuffer` of the contents as binary data.
  */
-function readFile (file) {
+function readFile(file: File): Promise<ArrayBuffer> {
   return new Promise((resolve, reject) => {
     const reader = new FileReader()
 
-    reader.onload = (e) => {
+    reader.onload = (e: FileReaderEvent) => {
       if (e.target.error) {
         reject(e.target.error)
       } else {
@@ -74,7 +88,7 @@ function readFile (file) {
 /**
  * Replaces the previously inserted placeholder image links in the `HTMLTextAreaElement`.
  */
-function replacePlaceholder (el, filename, url) {
+function replacePlaceholder(el: HTMLTextAreaElement, filename: string, url: string): void {
   let oldText = el.value
   let fileroot = filename.replace(/\.[^/.]+$/, '')
   let newText = oldText.replace(`![Uploading ${filename}...]()`, `![${fileroot}](${url})`)
@@ -87,14 +101,14 @@ function replacePlaceholder (el, filename, url) {
  *
  * Returns a `Promise` that resolves with the response text or rejects with the exception on errors.
  */
-function request (method, url, json) {
+function request(method: string, url: string, json: string): Promise<string> {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest()
 
     xhr.open(method, url)
 
     xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8')
-    xhr.onload = (e) => resolve(e.target.responseText)
+    xhr.onload = (e: XMLHttpRequestEvent) => resolve(e.target.responseText)
     xhr.onerror = (e) => reject(e)
 
     xhr.send(json)
@@ -104,7 +118,7 @@ function request (method, url, json) {
 /**
  * Uploads the given `File` asynchronously.
  */
-async function uploadFile (file) {
+async function uploadFile(file: File): Promise<string> {
   const buffer = await readFile(file)
   const base64 = arrayBufferToBase64(buffer)
   const payload = {base64: base64, mimeType: file.type}
@@ -119,17 +133,19 @@ async function uploadFile (file) {
 /**
  * Uploads the dropped files.
  */
-function uploadFiles (files) {
+function uploadFiles(files: FileList): Promise<void[]> {
   let promises = []
 
-  for (let file of files) {
-    insertPlaceholder(imageDropElement, file.name)
+  if (imageDropElement) {
+    for (let file of <File[]><any>files) {
+      insertPlaceholder(imageDropElement, file.name)
 
-    let promise = uploadFile(file).then((url) => {
-      replacePlaceholder(imageDropElement, file.name, url)
-    })
+      let promise = uploadFile(file).then((url) => {
+        replacePlaceholder(imageDropElement, file.name, url)
+      })
 
-    promises.push(promise)
+      promises.push(promise)
+    }
   }
 
   return Promise.all(promises)
