d6a8868e621ad5e3524fc3f365b7d6a1cf8d54da
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-01-25 20:42:41 -0800

    :art: Use Plug.Exception protocol to make things pretty!

---
+++ lib/staff_notes_api/authentication_error.ex
@@ -0,0 +1,32 @@
+defmodule StaffNotesApi.AuthenticationError do
+  @moduledoc """
+  Raised when API authentication fails.
+
+  This is raised when:
+
+  * The `Authorization` header is missing
+  * The `Authorization` header isn't formatted correctly
+  * The token in the header is invalid or expired
+  * The user ID in the token is invalid
+
+  This complies with the `Plug.Exception` protocol to automatically return a `401 Unauthorized` HTTP
+  status and message.
+
+  ## Examples
+
+  Raising with the generic error message:
+
+  ```
+  iex> raise StaffNotesApi.AuthenticationError
+  ** (StaffNotesApi.AuthenticationError) Authentication failed
+  ```
+
+  Raising with a custom error message:
+
+  ```
+  iex> raise StaffNotesApi.AuthenticationError, message: "Something went wrong"
+  ** (StaffNotesApi.AuthenticationError) Something went wrong
+  ```
+  """
+  defexception [message: "Authentication failed", plug_status: 401]
+end
--- lib/staff_notes_api/plugs/auth_token_plug.ex
+++ lib/staff_notes_api/plugs/auth_token_plug.ex
@@ -6,40 +6,35 @@ defmodule StaffNotesApi.AuthTokenPlug do
   `token [big-long-token-thing]`. When found, it verifies the token (using
   `Phoenix.Token.verify/4`). If the token is valid, it extracts the user ID from the token and
   verifies that there is a user in the database with the given user ID. If any step fails,
-  `401 Unauthorized` is returned and the request is halted.
+  a `StaffNotesApi.AuthenticationError` is raised which causes a `401 Unauthorized` HTTP
+  status to be returned with a message.
 
   ## Options
 
   * `:api_access_salt` -- Salt to use when verifying the token _(default: "api-access-salt")_
-  * `:max_age` -- Maximum allowable age of the token in seconds _(default: one day)_
+  * `:max_age` -- Maximum allowable age of the token in seconds _(default: one day or 86,400
+    seconds)_
   """
-  import Plug.Conn, only: [get_req_header: 2, halt: 1, send_resp: 3]
+  import Plug.Conn, only: [get_req_header: 2]
 
   alias Phoenix.Token
 
   alias StaffNotes.Accounts
+  alias StaffNotesApi.AuthenticationError
 
   @doc """
   Initialize the plug with the supplied options.
   """
-  def init(options) do
+  def init(options \\ []) do
     default_options()
     |> Keyword.merge(Application.get_env(:staff_notes, __MODULE__) || [])
-    |> Keyword.merge(options || [])
+    |> Keyword.merge(options)
   end
 
   @doc """
   Call the plug with the initialized options.
   """
-  def call(conn, options) do
-    case valid_token?(conn, options) do
-      true -> conn
-      false ->
-        conn
-        |> send_resp(:unauthorized, "")
-        |> halt()
-    end
-  end
+  def call(conn, options), do: validate_token!(conn, options)
 
   @doc """
   Gets the default options.
@@ -51,31 +46,37 @@ defmodule StaffNotesApi.AuthTokenPlug do
     ]
   end
 
-  defp valid_token?(conn, options) do
+  defp validate_token!(conn, options) do
     conn
-    |> verify_token(options)
-    |> verify_user()
+    |> verify_token!(options)
+    |> verify_user!()
+
+    conn
   end
 
-  defp verify_token(conn, options) do
+  defp verify_token!(conn, options) do
+    conn
+    |> get_token!(options)
+    |> do_verify_token!()
+  end
+
+  defp get_token!(conn, options) do
     case get_req_header(conn, "authorization") do
       ["token " <> token] -> Token.verify(conn, options[:api_access_salt], token, options)
-      _ -> {:error, "Invalid authorization header"}
+      _ -> raise AuthenticationError, message: "`Authorization` request header missing or malformed"
     end
   end
 
-  # If we can't verify the token for any reason, deny the request
-  defp verify_user({:error, _}), do: false
+  defp do_verify_token!({:ok, nil}), do: raise AuthenticationError, message: "Not logged in"
+  defp do_verify_token!({:ok, user_id}), do: user_id
+  defp do_verify_token!({:error, reason}) do
+    raise AuthenticationError, message: "Authentication token is #{reason}"
+  end
 
-  # Users who aren't logged in can't use the API
-  # TODO: We should probably log this in the future since logged out users shouldn't receive a token
-  defp verify_user({:ok, nil}), do: false
-
-  # We got a user ID, but is it real?
-  defp verify_user({:ok, id}) do
+  defp verify_user!(id) do
     case Accounts.get_user(id) do
-      nil -> false
-      _ -> true
+      nil -> raise AuthenticationError, message: "Authentication token is invalid"
+      _ -> nil
     end
   end
 end
